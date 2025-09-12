debf746375debcc087b9c8ccc94a4d912ff42e38
FerRojasChoco <feraulrap60@gmail.com>
2025-09-12 19:14:53 +0800

    Worked on packages, engine and window manager update for opening a window and handling functionalities

--- src/com/chocoEngine/Launcher.java
+++ src/com/chocoEngine/Launcher.java
@@ -1,17 +1,25 @@
 package com.chocoEngine;
 
+import com.chocoEngine.core.EngineManager;
 import com.chocoEngine.core.WindowManager;
+import com.chocoEngine.core.utils.Consts;
 
 public class Launcher{
+
+    private static WindowManager window;
+    private static EngineManager engine;
     public static void main(String[] args){
-        WindowManager window = new WindowManager("CHOCO ENGINE", 1600, 900, false);   
+        window = new WindowManager(Consts.TITLE, 1600, 900, false); 
+        engine = new EngineManager();  
         
-        window.init();
-
-        while(!window.windowShouldClose()){
-            window.update();
+        try{
+            engine.start();
+        } catch (Exception e){
+            e.printStackTrace();
         }
+    }
 
-        window.cleanup();
+    public static WindowManager getWindow(){
+        return window;
     }
 }
---
+++ src/com/chocoEngine/core/utils/Consts.java
@@ -0,0 +1,5 @@
+package com.chocoEngine.core.utils;
+
+public class Consts {
+    public static final String TITLE = "CHOCO ENGINE";
+}
